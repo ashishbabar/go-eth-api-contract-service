f87f8f1226c83b12c35389dcb82265f81586eda9
Ashish Babar <ashishbabar101@gmail.com>
2023-02-22 17:24:02 +0530

    Moved logger

---
+++ utils/logger.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"go.uber.org/zap"
+)
+
+var ZapLogger *zap.Logger
+
+func init() {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	ZapLogger, _ = config.Build()
+}
+func getLogger() *zap.Logger {
+	return ZapLogger
+}
